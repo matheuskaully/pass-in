fa3dbbf119aa800eff765a9119c6161db5c8a513
Matheus Kaúlly <kaully.santos@gmail.com>
2024-06-16 22:26:23 -0300

    refactor: tag <a> has transformed in a link component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import logo from '../assets/nlw-unit-icon.svg'
+import { NavLink } from './nav-link'
 
 export function Header() {
   return (
@@ -6,8 +7,12 @@ export function Header() {
       <img src={logo} alt="nlw unite" />
 
       <nav className='flex items-center gap-5'>
-        <a href="" className='font-medium text-sm text-zinc-300'>Eventos</a>
-        <a href="" className='font-medium text-sm'>Participantes</a>
+        <NavLink href='/'>
+          Eventos
+        </NavLink>
+        <NavLink href='/'>
+          Participantes
+        </NavLink>
       </nav>
     </div>
 
